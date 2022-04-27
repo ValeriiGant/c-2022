510fce21ad98b8ba3be4d2d319aba0f708db1481
Valerii Gant <4vpost@gmail.com>
2022-04-27 16:35:40 +0400

    Ex007_Magic

---
+++ Ex007_Magic/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1, 
+xb = 1, yb = 10, 
+xc = 10, yc = 10;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
